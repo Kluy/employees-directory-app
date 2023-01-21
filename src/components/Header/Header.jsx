b30ff8a12b4be6e9596d373fb254187cb009aa71
Kluy <kluy@i.ua>
2023-01-21 23:39:24 +0200

    c4_21-01

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import './header.scss';
 
 const Header = () => {
-  return <div>tutu</div>;
+  return (
+    <header className="header">
+      <div className="header__logo-box">
+        <img
+          className="header__logo"
+          src="https://iev.aero/_nuxt/img/logo@2x.2d2c20b.png"
+          alt=""
+        />
+      </div>
+      <ul className="header__nav">
+        <li className="nav__item">Пасажирам</li>
+        <li className="nav__item">Послуги IEV</li>
+        <li className="nav__item">VIP</li>
+        <li className="nav__item">Партнерам</li>
+        <li className="nav__item">Пресцентр</li>
+        <li className="header__language nav__item">UA</li>
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
