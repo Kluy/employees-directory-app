747de5c6dabfa2ab739e89a3f5ed85442498b15f
Kluy <kluy@i.ua>
2023-01-23 18:09:44 +0200

    c13_23-01

--- src/components/shedule/Shedule.jsx
+++ src/components/shedule/Shedule.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import './shedule.scss';
 
 const Shedule = () => {
-  return <div></div>;
+  return (
+    <section className="shedule">
+      <div className="filter">
+        <button className="filter__btn">ВИЛІТ</button>
+        <button className="filter__btn">ПРИЛІТ</button>
+      </div>
+    </section>
+  );
 };
 
 export default Shedule;
