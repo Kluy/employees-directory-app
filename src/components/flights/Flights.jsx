576a75b100773e41f63e72b658b3a8adeb563efe
Kluy <kluy@i.ua>
2023-01-26 14:19:02 +0200

    c3-2601

--- src/components/flights/Flights.jsx
+++ src/components/flights/Flights.jsx
@@ -11,8 +11,8 @@ const Flights = ({ shedule }) => {
         <li className="flight__item">Термінал</li>
         <li className="flight__item">Розклад</li>
         <li className="flight__item">Напрямок</li>
-        <li className="flight__status">Статус</li>
-        <li className="flight__airline">Авіакомпанія</li>
+        <li className="flight__item_status">Статус</li>
+        <li className="flight__item_airline">Авіакомпанія</li>
         <li className="flight__item">Рейс</li>
         <li className="flight__item"></li>
       </ul>
@@ -20,26 +20,38 @@ const Flights = ({ shedule }) => {
         shedule.body.departure.map((flight) => {
           return (
             <ul className="flights__table flight">
-              <li className="flight__terminal">{flight.term}</li>
+              <li
+                className={`flight__terminal ${
+                  flight.term === 'D' ? 'flight__terminal_D' : ''
+                }`}
+              >
+                {flight.term}
+              </li>
               <li className="flight__item">
                 {moment(flight.timeDepShedule).format('h:mm')}
               </li>
               <li className="flight__item">{flight['airportToID.city']}</li>
-              <li className="flight__status">
+              <li className="flight__item_status">
                 Вилетів о {moment(flight.timeDepFact).format('h:mm')}
               </li>
-              <li className="flight__airline">
+              <li className="flight__item_airline flight__airline">
                 <img
                   className="flight__airline-logo"
                   src={flight.airline.en.logoSmallName}
                   alt="logo"
                 />
-                <span> {flight.airline.ua.name}</span>
+                <span className="flight__airline-name">
+                  {flight.airline.ua.name}
+                </span>
               </li>
               <li className="flight__item">
                 {flight.codeShareData[0].codeShare}
               </li>
-              <li className="flight__item"></li>
+              <li className="flight__item ">
+                <a className="flight__item_details" href="#">
+                  Деталі рейсу
+                </a>
+              </li>
             </ul>
           );
         })}
