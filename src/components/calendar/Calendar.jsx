1d0fe5c5458aef5317686541353bcd7026764b82
Kluy <kluy@i.ua>
2023-02-02 12:27:37 +0200

    c5-0202

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import DateSelector from '../dateSelector/DateSelector';
+import DateSelector from './DateSelector';
 import PropTypes from 'prop-types';
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import { flightNumberAction, getSheduleAction } from '../shedule.actions';
