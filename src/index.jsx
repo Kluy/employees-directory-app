c5407c75027a458438b76f644981cc496baa1d07
Kluy <kluy@i.ua>
2023-02-15 14:45:14 +0200

    1_1502

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
-import './var.scss';
 
 ReactDOM.createRoot(document.querySelector('#root')).render(<App />);
