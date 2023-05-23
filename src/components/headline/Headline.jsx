1d333fa8205af844ed4044923730c8fa78fffc11
Kluy <kluy@i.ua>
2023-05-23 15:57:06 +0300

    3

--- src/components/headline/Headline.jsx
+++ src/components/headline/Headline.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import Button from '../button/Button';
 import './headline.scss';
 
 const Headline = () => {
-  return <section className="headline"></section>;
+  return (
+    <section className="headline">
+      <h1></h1>
+      <p1></p1>
+      <Button text="Sign Up" />
+    </section>
+  );
 };
 
 export default Headline;
