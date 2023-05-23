1b9c0cd2b8eb5ac51f31cec1afc6076c37e0cb52
Kluy <kluy@i.ua>
2023-05-23 15:54:32 +0300

    2

--- src/gateway/gateway.js
+++ src/gateway/gateway.js
@@ -1,7 +1,7 @@
-const baseUrl = 'https://api.iev.aero/api/flights/';
+const baseUrl = '';
 
-export const getFlights = (date) => {
+export const getFlights = date => {
   return fetch(`${baseUrl}${date}`)
-    .then((response) => response.json())
+    .then(response => response.json())
     .catch(() => console.log('Internal Server Error. Can"t display flights'));
 };
