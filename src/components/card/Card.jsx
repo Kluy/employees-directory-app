cb9998afa2d547086fd3d03cbefd79ef071b37ee
Kluy <kluy@i.ua>
2023-06-05 15:08:06 +0300

    preloader

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import Text from '../text/Text';
 import Image from '../image/Image';
 import './card.scss';
-import Preloader from '../preloader/Preloader';
 
 const Card = ({ photo, name, position, email, phone }) => {
   return (
     <li className="card">
-      <Preloader />
       <Image className="photo" src={photo} alt="User photo" />
       <Text className="p1_name" text={name} />
       <Text text={position} />
--- src/components/get/Get.jsx
+++ src/components/get/Get.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from '../button/Button';
 import Card from '../card/Card';
 import Heading from '../heading/Heading';
+import Preloader from '../preloader/Preloader';
 import { fetchUsers } from '../../gateway/gateway';
 import { useEffect } from 'react';
 import { useState } from 'react';
@@ -29,18 +30,22 @@ const Get = () => {
   return (
     <section className="get">
       <Heading text="Working with GET request" />
-      <ul className="cards">
-        {usersData.users.map(elem => (
-          <Card
-            key={elem.id}
-            photo={elem.photo}
-            name={elem.name}
-            position={elem.position}
-            email={elem.email}
-            phone={elem.phone}
-          />
-        ))}
-      </ul>
+      {usersData.users.length === 0 ? (
+        <Preloader />
+      ) : (
+        <ul className="cards">
+          {usersData.users.map(elem => (
+            <Card
+              key={elem.id}
+              photo={elem.photo}
+              name={elem.name}
+              position={elem.position}
+              email={elem.email}
+              phone={elem.phone}
+            />
+          ))}
+        </ul>
+      )}
       <Button
         hidden={usersData.page === usersData.totalPages + 1}
         className="button_show-more"
